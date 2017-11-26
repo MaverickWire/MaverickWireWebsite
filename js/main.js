4e9115a96521a5cd4597a5b470349f04ef95df14
EthanMoses <ethanmoses@rocketmail.com>
2017-11-26 17:33:19 -0600

    Minor bugfixes/improvements

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Automatic date for footer
+var today = new Date();
+var year = today.getFullYear();
+var el = document.getElementById('date');
+el.innerHTML = "Copyright &copy;" + year + ", Maverick Wire, LLC. All rights reserved.";
