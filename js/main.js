82764e999fced65c51cf816074c633fce26e4228
EthanMoses <ethanmoses@rocketmail.com>
2017-12-16 13:28:34 -0600

    remove map

--- js/main.js
+++ js/main.js
@@ -3,26 +3,8 @@ var today = new Date();
 var year = today.getFullYear();
 var el = document.getElementById('date');
 el.innerHTML = "Copyright &copy;" + year + ", Maverick Wire, LLC. All rights reserved.";
-
-// Google Maps API
-jQuery(function($) {
-    // Asynchronously Load the map API 
-    var script = document.createElement('script');
-    script.src = "//maps.googleapis.com/maps/api/js?key=AIzaSyBI_QjrjaE2Nj15hmjxVNO62sl-lbUzHzQ&callback=initMap";
-    document.body.appendChild(script);
-});
-
-function initialize() {
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
-                    
-    // Display a map on the page
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    map.setTilt(45);
-        
+     
+    /*
     // Multiple Markers
     var markers = [
         ['Sunlight Feed & Farm Center', 37.9424996, -90.782872],
@@ -55,36 +37,4 @@ function initialize() {
         '<p>209 East 8th Street<br>Rolla, MO 65401</p>' +
         '</div>']
     ];
-        
-    // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
-    // Loop through our array of markers & place each one on the map  a
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2], markers[i][3], markers[i][4], markers[i][5]);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0]
-        });
-        
-        // Allow each marker to have an info window    
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
-
-        // Automatically center the map fitting all markers on the screen
-        map.fitBounds(bounds);
-    }
-
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(8);
-        google.maps.event.removeListener(boundsListener);
-    });
-    
-}
+    */
